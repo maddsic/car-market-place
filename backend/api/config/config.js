9fab9d46361abbcc4304955989a6b28e9b9aceba
maddsic <sainsidibeh@yahoo.com>
2026-07-27 09:54:52 -0400

    fix: config.js railway MYSQLUSER name error

--- backend/api/config/config.js
+++ backend/api/config/config.js
@@ -1,24 +1,27 @@
 module.exports = {
   development: {
-    "username": process.env.MYSQL_USER || "admin",
+    "username": process.env.MYSQLUSER || "admin",
     "password": process.env.MYSQLPASSWORD || "password@1234",
     "database": process.env.MYSQLDATABASE || "carDB",
     "host": process.env.MYSQLHOST || "127.0.0.1",
+    "port": process.env.MYSQLPORT || 3306,
     "dialect": process.env.DB_DIALECT || "mysql",
     "logging": process.env.DB_LOGGING ? console.log : false
   },
   test: {
-    "username": process.env.MYSQL_USER || "root",
+    "username": process.env.MYSQLUSER || "root",
     "password": process.env.MYSQLPASSWORD,
     "database": process.env.MYSQLDATABASE || "database_test",
     "host": process.env.MYSQLHOST || "127.0.0.1",
+    "port": process.env.MYSQLPORT || 3306,
     "dialect": process.env.DB_DIALECT || "mysql"
   },
   production: {
-    "username": process.env.MYSQL_USER || "root",
+    "username": process.env.MYSQLUSER || "admin",
     "password": process.env.MYSQLPASSWORD,
-    "database": process.env.MYSQLDATABASE || "database_production",
+    "database": process.env.MYSQLDATABASE || "railway",
     "host": process.env.MYSQLHOST || "127.0.0.1",
+    "port": process.env.MYSQLPORT || 3306,
     "dialect": process.env.DB_DIALECT || "mysql"
   }
 }
