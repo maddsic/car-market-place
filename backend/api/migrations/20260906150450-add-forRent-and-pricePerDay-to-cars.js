ce859977019ca3b85a9a56a3010bf06d21ffbb43
maddsic <sainsidibeh@yahoo.com>
2026-09-06 13:18:16 -0400

    Fix: sequelize duplicate error for field drive

--- backend/api/migrations/20260906150450-add-forRent-and-pricePerDay-to-cars.js
+++ backend/api/migrations/20260906150450-add-forRent-and-pricePerDay-to-cars.js
@@ -2,6 +2,16 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    const tableInfo = await queryInterface.describeTable('Car');
+
+    // Duplicate Drive column check to avoid adding it again if it already exists
+    if (!tableInfo.drive) {
+      await queryInterface.addColumn('Car', 'drive', {
+        type: Sequelize.STRING,
+        allowNull: true,
+      })
+    }
+
     // 1. Add 'forRent' column (Boolean, defaults to false)
     await queryInterface.addColumn('Car', 'forRent', {
       type: Sequelize.BOOLEAN,
@@ -18,8 +28,9 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
+    const tableInfo = await queryInterface.describeTable('Car');
     // Revert changes if migration is rolled back
-    await queryInterface.removeColumn('Car', 'forRent');
-    await queryInterface.removeColumn('Car', 'pricePerDay');
+    if (tableInfo.forRent) await queryInterface.removeColumn('Car', 'forRent');
+    if (tableInfo.pricePerDay) await queryInterface.removeColumn('Car', 'pricePerDay');
   }
 };
