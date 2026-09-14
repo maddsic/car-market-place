f8120c2359e29727c926175344fad049ad322701
maddsic <sainsidibeh@yahoo.com>
2026-09-14 18:39:36 -0400

    Enhance: Send verification link logic

--- backend/api/authModule/authRepository.js
+++ backend/api/authModule/authRepository.js
@@ -5,13 +5,17 @@ class AuthRepository {
     this.User = models.User;
   }
 
+  // Find a user by their email address
   async findUserByEmail(email) {
     return this.User.findOne({ where: { email } });
   }
+
+  // Create a new user in the database
   async createUser(userData) {
     return this.User.create(userData);
   }
 
+  // Find a user by their verification token and ensure the token hasn't expired
   async findUserByVerificationToken(token) {
     const user = await User.findOne({
       verificationToken: token,
@@ -21,6 +25,7 @@ class AuthRepository {
     return user;
   };
 
+  // Update the user's verification status
   async updateUser(userId, updateData) {
     const [affectedCount] = await User.update(updateData, {
       where: { id }
@@ -38,7 +43,7 @@ class AuthRepository {
     )
   }
 
-
+  // Update the user's password and clear the reset code and its expiration
   async updateUserPasswordAndClearResetCode(email, newHashedPassword) {
     const [affectedRows] = await User.update(
       {
