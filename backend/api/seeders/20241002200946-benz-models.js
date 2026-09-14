4685f6d96f96049630c75ce3570a9f974b613f45
maddsic <sainsidibeh@yahoo.com>
2026-09-13 22:09:58 -0400

    Feature: add more car makes and models to the database

--- backend/api/seeders/20241002200946-benz-models.js
+++ backend/api/seeders/20241002200946-benz-models.js
@@ -4,7 +4,7 @@ const { v4: uuidv4 } = require('uuid');
 module.exports = {
   async up(queryInterface, Sequelize) {
     const [make] = await queryInterface.sequelize.query(
-      `SELECT id FROM CarMake WHERE name = 'Mercedez' LIMIT 1;`
+      `SELECT id FROM CarMake WHERE name = 'Mercedes' LIMIT 1;`
     );
 
     if (!make || make.length === 0) {
